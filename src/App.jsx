069a29a49b0c8ed81be8a58d9d8f5e6a66b3ba3f
Kris <kris051199@gmail.com>
2023-12-01 17:03:13 +0530

    setState2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,64 @@
 import React, {useState} from "react";
 
-const App = () => {
-        const [count,setCount] = useState(10);
-        const incrementCount = () =>{
-          setCount((count)=> {
-            return count+1
-          } )
-          setCount((count)=> {
-            return count+1
-          } )
-        }
-        const decrementCount = () =>{
-          setCount((count)=> {
-            return count-1
-          })
-          setCount((count)=> count-4
-          
-          )
-        }
-        
-  return (
+const App = () =>{
+  const initialObj = [
+    {
+      id: 1,
+      firstName: "Sudharshan",
+      lastName: "Badri",
+      age: 15
+    },
+    {
+      id: 2,
+      firstName: "Sudharshan",
+      lastName: "Badri",
+      age: 15
+    },
+    {
+      id: 3,
+      firstName: "Sudharshan",
+      lastName: "Badri",
+      age: 15
+    },
+    {
+      id: 4,
+      firstName: "Sudharshan",
+      lastName: "Badri",
+      age: 15
+    }
+];
+
+  const [data,setData] = useState(initialObj)
+
+    console.log()
+  const deleteProfile = (id) =>{
+    const filterData = data.filter((eachItem)=>{
+      return eachItem.id !== id
+    })
+   console.log(filterData)
+   setData(filterData)
+  }
+  
+   return(
     <div>
-      <button onClick={decrementCount}>decrimentCount</button>
-      <span>count: {count}</span>
-      <button onClick = {incrementCount}>incrementCount</button>
+        <ul>
+        {
+          data.map((eachItem,index) => {
+            const {id, firstName, lastName, age} = eachItem
+              return <li key = {index}>
+                          <div> firstName : {firstName}</div>
+                          <div> lastName : {lastName}</div>
+                          <div> age : {age}</div>
+                          <button onClick = {()=> deleteProfile(id)}>del lastNam</button>
+                     </li>
+          })
+         }</ul>
     </div>
-  )
+   )
+
+  
 }
+
+
 
 export default App;
