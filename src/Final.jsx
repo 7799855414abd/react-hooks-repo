8648cd2bd5abacb7788a9a758cbcd13629a7252f
Kris <kris051199@gmail.com>
2023-12-02 15:38:33 +0530

    useEffect() hook-final

--- src/Final.jsx
+++ src/Final.jsx
@@ -4,12 +4,29 @@ import React,{useEffect,useState} from "react";
 
 const Final = () => {
     const [count,setCount] = useState(0);
+    const [status,setStatus] = useState(true);
+    const [pagewidth, setPagewidth] = useState(window.innerHeight)
 
   useEffect(()=> {
-    console.log("i am use effect",count)
-   },[])
-  return <div className="container">
+     const resizeHandler = () => {
+      setPagewidth(window.innerWidth)
+      }
+      window.addEventListener("resize",resizeHandler);
+      console.log("i am from useEffect", count);
+
+      return()=>{
+        console.log("I am removing");
+        window.removeEventListener("resize",resizeHandler);
+      };
+    
+   })
+  
+  
+  
+  
+   return <div className="container">
    <h1>use Effect test</h1>
+   <div onClick={()=> setStatus(!status)}>{status?"show":"hide"}</div>
    <h1>{count}</h1>
    <button onClick={()=> setCount(count+1)}>+</button>
    </div>
